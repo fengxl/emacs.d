28b7be98ea38204186f5e1bf1a9fd7f3d62cac78
Oliver Cui <xnl@OliverdeMacBook-Pro.local>
2016-04-02 10:38:56 +0800

    Basic support

---
+++ lisp/init-rust.el
@@ -0,0 +1,35 @@
+;; Basic rust support
+
+;;; Code:
+(require-package 'rust-mode)
+(require-package 'flycheck-rust)
+(require-package 'racer)
+(require-package 'ac-racer)
+(require-package 'rustfmt)
+(require-package 'cargo)
+
+;;(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+;;(setq racer-cmd (expand-file-name "~/.cargo/bin/racer"))
+;;(setq racer-rust-src-path (expand-file-name "~/code/rust/rust/src/"))
+
+;; Use "M-x rustfmt-format-buffer" to format code
+;; Bind key to fmt
+;; (define-key rust-mode-map (kbd "C-c C-f") #'rustfmt-format-buffer)
+
+;;run rustfmt before saving rust buffers:
+;;(add-hook 'rust-mode-hook #'rustfmt-enable-on-save)
+;;(add-hook 'rust-mode-hook #'flycheck-rust-setup)
+;;(add-hook 'rust-mode-hook #'racer-mode)
+;;(add-hook 'racer-mode-hook #'eldoc-mode)
+;;(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+;;
+(defun my/racer-mode-hook ()
+  (ac-racer-setup))
+;;(add-hook 'racer-mode-hook 'my/racer-mode-hook)
+
+
+(provide 'init-rust)
+
+;;; init-rust.el  ends here
